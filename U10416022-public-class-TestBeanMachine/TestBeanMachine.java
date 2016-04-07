caf389dbbe9c06a9a0a3d3f47d4f9204f5c64a16
HongYingFang <zx04785@gmail.com>
2016-04-07 14:02:30 +0800

    Delete TestBeanMachine.java

--- U10416022-public-class-TestBeanMachine/TestBeanMachine.java
+++
@@ -1,9 +0,0 @@
-//U10416022
-public class TestBeanMachine{
-	//main method
-	public static void main(String[] args) {
-		
-		javafx.application.Application.launch(BeanMachine.class);	
-            	
-	}
-}
