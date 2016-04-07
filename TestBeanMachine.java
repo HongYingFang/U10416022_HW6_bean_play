9cefde1afbfb7af869a8bd7d0ab5cc6b78553aa6
HongYingFang <zx04785@gmail.com>
2016-04-07 14:03:50 +0800

    Create TestBeanMachine.java
    This program is used to call the BeanMachine.class.

---
+++ TestBeanMachine.java
@@ -0,0 +1,9 @@
+//U10416022
+public class TestBeanMachine{
+	//main method
+	public static void main(String[] args) {
+		
+		javafx.application.Application.launch(BeanMachine.class);	
+            	
+	}
+}
